11efdc3c5d387fb1a1833bb85f4adeb62313203e
Alex <alext@DESKTOP-942LP8A>
2025-03-31 13:01:36 +0100

    Modules configuration and updated TF

--- aws_TF_AI_VPC.tf
+++ aws_TF_AI_VPC.tf
@@ -1,6 +1,12 @@
 ################################################################
-# Terraform buildout code for AI VPCs using EC2 & Packer      #
+# Terraform buildout code for AI VPCs using EC2, Packer & Kafka #
 ################################################################
+
+
+################################################
+#    AWS Configuration, EC2 build, installation#
+#              and deployment                  #
+################################################
 
 # Define the provider
 provider "aws" {
@@ -12,11 +18,18 @@ resource "aws_vpc" "TF_AI_VPC" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Add the subnet to the VPC
-resource "aws_subnet" "TF_AI_Subnet" {
-  vpc_id     = aws_vpc.TF_AI_VPC.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+# Create a public subnet
+resource "aws_subnet" "TF_AI_Public_Subnet" {
+  vpc_id                  = aws_vpc.TF_AI_VPC.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true  # Public IPs enabled
+}
+
+# Create a private subnet
+resource "aws_subnet" "TF_AI_Private_Subnet" {
+  vpc_id                  = aws_vpc.TF_AI_VPC.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = false  # No public IPs
 }
 
 # Add the Internet Gateway
@@ -24,32 +37,56 @@ resource "aws_internet_gateway" "TF_AI_IGW" {
   vpc_id = aws_vpc.TF_AI_VPC.id
 }
 
-# Add a route table for public access
+# Allocate an Elastic IP for NAT Gateway
+resource "aws_eip" "TF_AI_NAT_EIP" {
+  domain = "vpc"
+}
+
+# Create the NAT Gateway
+resource "aws_nat_gateway" "TF_AI_NAT" {
+  allocation_id = aws_eip.TF_AI_NAT_EIP.id
+  subnet_id     = aws_subnet.TF_AI_Public_Subnet.id
+}
+
+# Public Route Table
 resource "aws_route_table" "TF_AI_Public_RT" {
   vpc_id = aws_vpc.TF_AI_VPC.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.TF_AI_IGW.id
   }
 }
 
+# Associate public subnet with public route table
 resource "aws_route_table_association" "TF_AI_Public_Assoc" {
-  subnet_id      = aws_subnet.TF_AI_Subnet.id
+  subnet_id      = aws_subnet.TF_AI_Public_Subnet.id
   route_table_id = aws_route_table.TF_AI_Public_RT.id
 }
 
-# Add the Security Group to the VPC
+# Private Route Table
+resource "aws_route_table" "TF_AI_Private_RT" {
+  vpc_id = aws_vpc.TF_AI_VPC.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.TF_AI_NAT.id
+  }
+}
+
+# Associate private subnet with private route table
+resource "aws_route_table_association" "TF_AI_Private_Assoc" {
+  subnet_id      = aws_subnet.TF_AI_Private_Subnet.id
+  route_table_id = aws_route_table.TF_AI_Private_RT.id
+}
+
+# Security Group allowing SSH access
 resource "aws_security_group" "allow_ssh" {
   vpc_id = aws_vpc.TF_AI_VPC.id
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -58,44 +95,28 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-# Create an Auto Scaling Group for Kubernetes Nodes
-resource "aws_launch_template" "TF_AI_K8S_LT" {
-  name_prefix   = "TF_AI_K8S_"
-  image_id      = "ami-0a94c8e4ca2674d5a"  # Replace with Packer-built AMI ID
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "TF_AI_K8S_Node"
-    }
-  }
-}
 
-resource "aws_autoscaling_group" "TF_AI_K8S_ASG" {
-  vpc_zone_identifier  = [aws_subnet.TF_AI_Subnet.id]
-  desired_capacity     = 2
-  min_size            = 1
-  max_size            = 5
-  launch_template {
-    id      = aws_launch_template.TF_AI_K8S_LT.id
-    version = "$Latest"
-  }
-}
+###############################
+#   Module Definitions        #
+###############################
 
-# Outputs
-output "vpc_id" {
-  value = aws_vpc.TF_AI_VPC.id
-}
 
-output "subnet_id" {
-  value = aws_subnet.TF_AI_Subnet.id
-}
+Module "Kafka_deploy"{
 
-output "security_group_id" {
-  value = aws_security_group.allow_ssh.id
 }
-
-output "autoscaling_group_name" {
-  value = aws_autoscaling_group.TF_AI_K8S_ASG.id
+# Deploy Kafka using Helm
+resource "helm_release" "kafka" {
+  name       = "kafka"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "kafka"
+  namespace  = "kafka"
+  create_namespace = true
+  values = [<<EOF
+replicaCount: 3
+externalAccess:
+  enabled: false
+  service:
+    type: ClusterIP
+EOF
+  ]
 }
